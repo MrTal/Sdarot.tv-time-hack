e9504b9ee52111efd1fa27122a5a46d4740094f8
yeo4 <yeo4.jo@gmail.com>
2018-06-29 13:55:12 +0300

    Update Tampermonkey_Script.js

--- Tampermonkey_Script.js
+++ Tampermonkey_Script.js
@@ -4,6 +4,8 @@
 // @grant    none
 // @include http://sdarot.*/watch/*/season/*/episode/*
 // @include https://sdarot.*/watch/*/season/*/episode/*
+// @include http://*.sdarot.*/watch/*/season/*/episode/*
+// @include https://*.sdarot.*/watch/*/season/*/episode/*
 // @encoding utf-8
 // @require http://code.jquery.com/jquery-latest.js
 // @require https://raw.githubusercontent.com/yeo4/Sdarot.tv-time-hack/master/HackTime_Sdarot.js
