cc13ee88921bc04a3e824ab91dae98ba03676100
yeo4 <yeo4.jo@gmail.com>
2018-06-29 10:31:06 +0300

    Create Tampermonkey_Script.js

---
+++ Tampermonkey_Script.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name     HackTime Sdarot
+// @version  0.2
+// @grant    none
+// @include http://sdarot.*/watch/*/season/*/episode/*
+// @include https://sdarot.*/watch/*/season/*/episode/*
+// @encoding utf-8
+// @require http://code.jquery.com/jquery-latest.js
+// @require https://raw.githubusercontent.com/yeo4/Sdarot.tv-time-hack/master/HackTime_Sdarot.js
+// @run-at      document-start
+// ==/UserScript==
